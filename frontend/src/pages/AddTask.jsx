69b1f3020b435f01946ae53df764a0ef57380720
mattiacoli <mattia.coli@aol.com>
2025-07-01 11:47:41 +0200

    Refactor AddTask component layout and implement controlled input for title

--- frontend/src/pages/AddTask.jsx
+++ frontend/src/pages/AddTask.jsx
@@ -1,5 +1,52 @@
+import { useState, useRef } from 'react'
+
+const symbols = "!@#$%^&*()-_=+[]{}|;:'\\\",.<>?/`~";
+
 export default function AddTask() {
+  // controlled input
+  const [title, setTitle] = useState('')
+
+  // uncontrolled input
+  const descriptionRef = useRef()
+  const statusRef = useRef()
+
+
   return (
-    <h1>AddTask</h1>
+    <div className="container">
+      <h1 className="text-center">AddTask</h1>
+
+      <div className="container my-4">
+        <form action="">
+          <div className="mb-3">
+
+            <input
+              className='form-control'
+              type="text"
+              name="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="titolo" />
+          </div>
+
+          <div className="mb-3">
+            <textarea
+              ref={descriptionRef}
+              name="description"
+              placeholder="descrizione"
+              className='form-control' />
+          </div>
+
+
+          <div className="mb-3 d-flex gap-2">
+            <label htmlFor="staus">Stato</label>
+            <select name="status" id="status" ref={statusRef} className='form-select'>
+              <option value="todo">To do</option>
+              <option value="doing">Doing</option>
+              <option value="done">Done</option>
+            </select>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
